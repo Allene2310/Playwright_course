10f23ea40a2cbf19b523ba0812c4e0d30c76b818
Allene2310 <yurchenko.yelena@gmail.com>
2025-10-16 21:52:34 +0300

    added steps to createOrder.spec.ts using filter and locator operators

--- tests/createOrder.spec.ts
+++ tests/createOrder.spec.ts
@@ -3,12 +3,20 @@ import { test, expect } from '@playwright/test';
 test('create an order', async ({ page }) => {
   await page.goto('https://demo.opencart.ua/index.php?route=common/home');
   await page.getByText('MacBook').click();
-  await page.getByRole('button', { name: 'В кошик' }).click();
+  await page.getByText('В кошик').or(page.locator('#button-cart')).click();
+  //await page.getByRole('button', { name: 'В кошик' }).click();
   await page.getByRole('link', { name: ' Кошик' }).click();
   await page.getByRole('link', { name: 'Продовжити покупки' }).click();
-  await page.getByRole('heading', { name: 'MacBook' }).getByRole('link').click();
-  await page.getByRole('button', { name: 'В кошик'  }).click();
-  await page.getByRole('link', { name: ' Кошик' }).click();
+  await page
+    .locator('.product-thumb')
+    .filter({ hasText: 'iPhone' })
+    .getByRole('heading', { name: 'iPhone' }) 
+    .getByRole('link')
+    .click();
+  await page.getByRole('button').and(page.locator('#button-cart')).click();
+  //await page.pause(); 
+  //await page.getByRole('button', { name: 'В кошик' }).click();
+  //await page.getByRole('link', { name: ' Кошик' }).click();
   await page.getByRole('link', { name: ' Оформлення замовлення' }).click();
   await page.getByRole('textbox', { name: 'E-Mail' }).click();
   await page.getByRole('textbox', { name: 'E-Mail' }).fill('l.example@gmail.com');
